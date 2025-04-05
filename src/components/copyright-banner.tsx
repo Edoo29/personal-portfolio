f67e9e17e32fa9e0e70e6b8053c02d6eba359a92
Edoo29 <edoardo.zaffaroni@gmail.com>
2025-04-05 15:04:57 +0200

    fixed copyright banner

--- src/components/copyright-banner.tsx
+++ src/components/copyright-banner.tsx
@@ -2,7 +2,7 @@ import { Copyright } from "lucide-react";
 
 function CopyrightBanner() {
   return (
-    <div className="w-full p-3 flex items-center justify-center gap-3 bg-black">
+    <div className="w-full p-3 flex items-center justify-center gap-3 bg-black absolute z-50">
       <Copyright className="text-[var(--color-shadows)]" />
       <p className="text-[var(--color-shadows)]">
         2025 Edoardo Zaffaroni - All rights reserved
