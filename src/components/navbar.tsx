f8ee6b19852a0cb2519bca091fb0abfca8ad558f
Edoo29 <edoardo.zaffaroni@gmail.com>
2025-04-05 13:19:40 +0200

    added github profile link

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,6 +2,7 @@ import { useTranslation } from "react-i18next";
 import Translate from "./translate";
 import LoadAnimation from "./load-animation";
 import { useState } from "react";
+import { Github } from "lucide-react";
 
 type NavbarItems = {
   label: string;
@@ -38,6 +39,15 @@ function Navbar() {
     <>
       <LoadAnimation delay={0.2}>
         <nav className="hidden md:flex text-[var(--color-shadows)]">
+          <a
+            href="https://github.com/Edoo29"
+            target="_blank"
+            rel="noreferrer"
+            title="My GitHub profile link"
+            className="flex gap-5 fixed left-0 top-0 mt-5 ml-5 p-3 border-glass rounded-xl backdrop-blur-md z-10 glowing-effect cursor-pointer"
+          >
+            <Github />
+          </a>
           <ul className="flex gap-7 justify-center mx-auto fixed z-10 top-0 left-1/2 transform -translate-x-1/2 p-3 mt-5 border-glass rounded-xl backdrop-blur-md">
             {navbarItems.map((item) => (
               <NavbarElement key={item.label} {...item} />
